ec94ae42d8f71420406e12fbceb64992bce3d59b
Piotr Plywacz <piotrus.plywacz@gmail.com>
2024-05-10 21:11:04 +0200

    Add argument parsing in client

--- kierki-klient.cpp
+++ kierki-klient.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
+#include <string>
 
 #include "common.h"
 #include "messages.h"
 #include "err.h"
 
-int main() {
+using namespace std;
+
+void parse_arguments(int argc, char* argv[], const char **host, uint16_t *port, bool *useIPv4,
+                     bool *useIPv6, bool *isAutoPlayer, char *seat) {
+
+    bool wasHostSet = false, wasPortSet = false, wasSeatSet = false;
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+        if (arg == "-h") {
+            if (i + 1 >= argc) fatal("missing argument for -h");
+            else {
+                *host = argv[i + 1];
+                wasHostSet = true;
+                i++;
+            }
+        } 
+        else if (arg == "-p") {
+            if (i + 1 >= argc) fatal("missing argument for -p");
+            else {
+                *port = read_port(argv[i + 1]);
+                wasPortSet = true;
+                i++;
+            }
+        }
+        else if (arg == "-4") *useIPv4 = true;
+        else if (arg == "-6") *useIPv6 = true;
+        else if (arg == "-N" || arg == "-E" || arg == "-S" || arg == "-W") {
+            wasSeatSet = true;
+            *seat = arg[1];
+        }
+        else if (arg == "-a") *isAutoPlayer = true;
+        else fatal("unknown argument");
+    }
+    if (!wasHostSet) fatal("missing -h argument");
+    if (!wasPortSet) fatal("missing -p argument");
+    if (!wasSeatSet) fatal("missing seat argument");    
+}
+
+int main(int argc, char* argv[]) {
+    const char *host;
+    uint16_t port;
+    bool useIPv4 = false, useIPv6 = false, isAutoPlayer = false;
+    char seat;
+    parse_arguments(argc, argv, &host, &port, &useIPv4, &useIPv6, &isAutoPlayer, &seat);
+    
+
+   return 0;
 }

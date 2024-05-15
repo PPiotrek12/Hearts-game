4ff361f83ed277413f69a691cfa75c0c85643bb2
Piotr Plywacz <piotrus.plywacz@gmail.com>
2024-05-15 15:19:05 +0200

    Server starting point: add Listener class to wrap poll() with timeouts

--- kierki-serwer.cpp
+++ kierki-serwer.cpp
@@ -10,6 +10,9 @@
 #include <poll.h>
 #include <signal.h>
 #include <fcntl.h>
+#include <algorithm>
+#include <chrono>
+#include <sys/time.h>
 
 #include "common.h"
 #include "messages.h"
@@ -54,66 +57,44 @@ void parse_arguments(int argc, char* argv[], uint16_t *port, bool *wasPortSet, s
     if (!wasFileSet) fatal("missing -f argument");
 }
 
-// Returns 0 if client was joined to the game, 1 if client was not joined to the game.
-int join_client_to_game(pollfd *fds, shared_ptr<Game_stage_server> game, int i) {
-    message mess = read_message(fds[i].fd); // TODO: timeout
-    if (mess.is_iam) {
-        if (seat_to_int(mess.iam.player) != -1 && !game->occupied[seat_to_int(mess.iam.player)]) {
-            game->occupied[seat_to_int(mess.iam.player)] = true;
-            game->how_many_occupied++;
-            fds[seat_to_int(mess.iam.player)].fd = fds[i].fd;
-            return 0;
-        }
-        else
-            game->send_busy(fds[i].fd);
-    }
-    cout<<"Client not joined to the game.\n";
-    close(fds[i].fd);
-    fds[i].fd = -1;
-    return 1;
-}
+struct Timeout_fd {
+    int fd;
+    int timeout; // In milliseconds, -1 if no timeout.
+    int revents;
+};
 
-void main_server_loop(pollfd *fds, Game_scenario game_scenario, int timeout) {
-    timeval to = {.tv_sec = timeout, .tv_usec = 0};
-    shared_ptr<Game_stage_server> game = make_shared<Game_stage_server>();
-    while (true) {
-        for (int i = 0; i < MAX_CLIENTS; i++) fds[i].revents = 0;
-        cout<<"czekam\n";
-        int poll_status = poll(fds, MAX_CLIENTS, -1);
-        cout<<"wchodze\n";
+struct Listener {
+    vector <Timeout_fd> clients;
+    Timeout_fd accepts;
+    void wrapped_poll() {
+        pollfd fds[clients.size() + 1];
+        fds[0] = {.fd = accepts.fd, .events = POLLIN, .revents = 0};
+        int min_timeout = -1;
+        for (int i = 0; i < (int)clients.size(); i++) {
+            fds[i + 1] = {.fd = clients[i].fd, .events = POLLIN, .revents = 0};
+            if (clients[i].timeout != -1) {
+                if (min_timeout == -1) min_timeout = clients[i].timeout;
+                else min_timeout = min(min_timeout, clients[i].timeout);
+            }
+        }
+        // Start measuring time.
+        auto beg_time = chrono::system_clock::now();
+
+        int poll_status = poll(fds, clients.size() + 1, min_timeout);
         if (poll_status < 0) syserr("poll");
-        if (poll_status == 0) continue;
 
-        if (fds[4].revents & (POLLIN | POLLERR)) { // New client.
-            sockaddr_in client_address;
-            socklen_t client_address_len = sizeof(client_address);
-            int client_fd = accept(fds[4].fd, (sockaddr*)&client_address, &client_address_len);
-            if (client_fd < 0) syserr("accept");
-            
-            for (int i = 5; i < MAX_CLIENTS; i++) { // Add new not playing client.
-                if (fds[i].fd == -1) {
-                    fds[i].fd = client_fd;
-                    fds[i].events = POLLIN;
-                    // Set timeout for the client.
-                    if (setsockopt(fds[i].fd, SOL_SOCKET, SO_RCVTIMEO, &to, sizeof(to)) < 0)
-                        syserr("setsockopt");
-                    if (fcntl(fds[i].fd, F_SETFL, O_NONBLOCK) < 0) syserr("fcntl"); // TODO: czy to potrzebne?
-                    break;
-                }
-            }
-            cout << "New client connected.\n";
-        }
-        for (int i = 5; i < MAX_CLIENTS; i++) { // Not playing clients.
-            if (fds[i].revents & (POLLIN | POLLERR)) { 
-                // Try to join client to the game.
-                if (join_client_to_game(fds, game, i)) continue;
-                cout<< "Client joined to the game.\n";
-                cout << "How many occupied: " << game->how_many_occupied << "\n";
-                // If all players are joined to the game, start the game.
-            }
+        // End measuring time and calculate duration in milliseconds.
+        auto end_time = chrono::system_clock::now();
+        chrono::duration<double> elapsed_seconds = end_time - beg_time;
+        int milliseconds = elapsed_seconds.count() * 1000;
+
+        // Update timeouts and revents.
+        for (int i = 0; i < (int)clients.size(); i++) {
+            if (clients[i].timeout != -1) clients[i].timeout -= milliseconds;
+            clients[i].revents = fds[i + 1].revents;
         }
     }
-}
+};
 
 int main(int argc, char* argv[]) {
     signal(SIGPIPE, SIG_IGN);
@@ -136,16 +117,4 @@ int main(int argc, char* argv[]) {
     if (bind(socket_fd, (sockaddr*)&server_address, sizeof(server_address)) < 0) syserr("bind");
 
     if (listen(socket_fd, QUEUE_LENGTH) < 0) syserr("listen");
-
-    // Sockets 0-3 are for playing clients in clockwise order. 
-    // Socket 4 is for accepting new clients. 
-    // All other sockets (4-(MAX_CLIENTS-1)) are for not playing (yet) clients.
-    pollfd fds[MAX_CLIENTS];
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        fds[i].fd = -1;
-        fds[i].events = POLLIN;
-    }
-    fds[4].fd = socket_fd;
-
-    main_server_loop(fds, game_scenario, timeout);
 }
